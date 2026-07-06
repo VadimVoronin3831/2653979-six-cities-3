ac28dc5659ce50fce9287db813510d4f81612b13
Vadim Voron <vadim.voronin.r@gmail.com>
2026-07-07 00:35:19 +0300

    выгрузка на хостинг

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/six-cities/',
+  base: '/2653979-six-cities-3/',
   test: {
     globals: true,
     environment: 'jsdom',
